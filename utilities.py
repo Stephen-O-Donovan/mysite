6c919bb8e402823647c0e57bb14bb428bd9ee8d9
Joey de l'Arago <joeydelarago@gmail.com>
2019-02-21 16:39:37 +0000

    Send email functionality added

--- utilities.py
+++ utilities.py
@@ -1,4 +1,7 @@
 import pymysql
+import sendgrid
+from sendgrid.helpers.mail import *
+import os
 
 ALLOWED_EXTENSIONS = set(['pdf', 'odt', 'txt'])
 def create_connection():
@@ -13,3 +16,15 @@ def create_connection():
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def send_email(to_email, subject, content):
+    sg = sendgrid.SendGridAPIClient(apikey='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')
+    from_email = Email("SFI@groupproject.com")
+    to_email = Email(to_email)
+    subject = subject
+    content = Content("text/plain", content)
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
