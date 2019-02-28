17a3e65fab47bf9c0646d2c2ceb611a9b91de8bc
Steve <102554291@umail.ucc.ie>
2019-02-28 18:25:58 +0000

    undid error with dashboardnav

--- app.py
+++ app.py
@@ -137,24 +137,24 @@ def adminProfile():
     form = BasicProfileForm(request.form)
     if 'email' in session:
         email = session['email']
-    if request.method == 'POST':
-        connection = create_connection()
-        with connection.cursor() as cursor:
-            if request.form['submit'] == 'Save Personal Info':
-                cursor.execute('UPDATE Users SET first_name=%s, surname=%s, suffix=%s,'
-                               'phone=%s, phone_extension=%s'
-                               'WHERE email=%s',
-                               [form1.first_name.data, form1.surname.data, form1.suffix.data,
-                               form1.phone.data, form1.phone_extension.data, email])
-                connection.commit()
 
     try:
         connection = create_connection()
         with connection.cursor() as cursor:
 
+            if request.method == 'POST':
+                if request.form['submit'] == 'Save Personal Info':
+                    cursor.execute('UPDATE Users SET first_name=%s, surname=%s, suffix=%s,'
+                                   'phone=%s, phone_extension=%s'
+                                   'WHERE email=%s',
+                                   [form.first_name.data, form.surname.data, form.suffix.data,
+                                   form.phone.data, form.phone_extension.data, email])
+                    connection.commit()
+
             #get data from tables
             cursor.execute('SELECT * FROM Users WHERE email = %s', [email])
             p1_data = cursor.fetchall()
+
     finally:
         connection.close()
 
