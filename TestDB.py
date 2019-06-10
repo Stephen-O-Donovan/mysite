e99cf3895d2f55e0c90c41cbd4ea8b1817900b85
Stephen O'Donovan <booklad@gmail.com>
2019-06-10 11:48:22 +0100

    Update TestDB.py

--- TestDB.py
+++ TestDB.py
@@ -1,10 +1,10 @@
 import pymysql.cursors
 
 # Connect to the database
-connection = pymysql.connect(host='mysql.netsoc.co',
-                             user='stephenteam5',
-                             password='eFDhNR4lUP',
-                             db='stephenteam5_Project5',
+connection = pymysql.connect(host='',
+                             user='',
+                             password='',
+                             db='',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor)
 
