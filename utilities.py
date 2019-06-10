1765ac67cd2194db04a805a1f8106ad9334c07e9
Stephen O'Donovan <booklad@gmail.com>
2019-06-10 11:47:43 +0100

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -6,10 +6,10 @@ from sendgrid.helpers.mail import Email, Content, Mail
 
 ALLOWED_EXTENSIONS = set(['pdf', 'odt', 'txt'])
 def create_connection():
-    connection = pymysql.connect(host='mysql.netsoc.co',
-                                 user='stephenteam5',
-                                 password='eFDhNR4lUP',
-                                 db='stephenteam5_Project5',
+    connection = pymysql.connect(host='',
+                                 user='',
+                                 password='',
+                                 db='',
                                  charset='utf8mb4',
                                  cursorclass=pymysql.cursors.DictCursor)
     return connection
